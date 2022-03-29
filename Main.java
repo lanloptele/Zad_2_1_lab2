d09ff45a50da9db0f266a8847680082c6650efdd
lanloptele <100698914+lanloptele@users.noreply.github.com>
2022-03-29 12:15:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    
+    int i=0;
+    while(i<5){ 
+      i++;
+      for(int y=0;y<i;y++)
+        System.out.print("*");
+        System.out.println();
+
+      }
+    }
+    
+  }
